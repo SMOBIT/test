abbe2882bd5a87da247212e13399dc4f86de1176
SMOBIT <Christian@smobit.de>
2025-04-14 06:30:53 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -5,8 +5,8 @@ window.addEventListener("load", async () => {
     return;
   }
 
-  const configUrl = `https://smobit.github.io/chatbot/config/${client}.json`;
-  const styleUrl = `https://smobit.github.io/chatbot/styles/${client}.css`;
+  const configUrl = `https://smobit.github.io/test/config/${client}.json`;
+  const styleUrl = `https://smobit.github.io/test/styles/${client}.css`;
 
   const style = document.createElement("link");
   style.rel = "stylesheet";
